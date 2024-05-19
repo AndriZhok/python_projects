f74b63d6e99ba26f90a1d8b507296742c0f887ea
AndriZhok <andri195595@gmail.com>
2024-05-19 20:09:29 +0300

    l

---
+++ learning_log/users/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
+
+def register(request):
+    if request.method != 'POST':
+        # Показати порожню форму реєстрації
+        form = UserCreationForm()
+    else:
+        # Опрацювати заповнену форму
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Авторизація та скерування на головну сторінку
+            login(request, new_user)
+            return redirect('learning_logs:index')
+    # Показати порожню форму
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
