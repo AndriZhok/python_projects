f69b6c8e52ab674bc1ea27fd32ce392f81ec6220
AndriZhok <andri195595@gmail.com>
2024-05-19 20:09:12 +0300

    l

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 
+from learning_log.users import views
+
 app_name = 'users'
 
 urlpatterns = [
     # Додати уставні URL для автентифікації
     path('', include('django.contrib.auth.urls')),
+    # Сторінка реєстрації.
+    path('register/', views.register, name='register'),
 ]
